59f4105942e7234a776dfc65bb08ff89d75f8556
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2026-01-08 23:48:18 +0530

    Created AddTask Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import List from './components/List'
+import AddTask from './components/AddTask'
 
 function App() {
 
   return (
     <>
       <h1>To-do App</h1>
+      <AddTask />
       <List task="Complete Project" />
     </>
   )
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddTask(){
+    return(
+        <div className="add-task">
+            <input type="text" name="" id="" />
+            <input type="button" value="Add" />
+        </div>
+    )
+}
+
+export default AddTask;
