5a25f2ebbd449b2193c72daa975c38200f65ba6d
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2026-01-12 22:19:31 +0530

    Added clear input logic

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 function AddTask(props){
     const [inputText, setInputText] = useState("");
-
     function handleChange(e){
         const newValue = e.target.value;
         setInputText(newValue);
     }
-    
     return(
         <div className="add-task">
-            <input type="text" name="" id="task-input" onChange={handleChange} />
+            <input type="text" name="" id="task-input" onChange={handleChange} value={inputText} />
             <input type="button" value="Add" className="add-btn" 
                 onClick={() => {props.onAdd(inputText);setInputText("");}} 
             />
